b3f9db6b24bfbf25a5c8690cdc5135e73d713c8d
TuHcopycoder <106220038@sv1.dut.udn.vn>
2026-05-09 14:00:38 +0700

    Update ldr_logic.h

--- main/ldr_logic.h
+++ main/ldr_logic.h
@@ -3,11 +3,7 @@
 #define SUN_LIMIT   400
 #include "esp_adc/adc_oneshot.h"
 
-/**
- * Cấu hình Kênh ADC cho ESP32-S3 (ADC1)
- * Lưu ý: Channel != GPIO Number
- */
-// ldr_logic.h — SỬA LẠI
+
 #define LDR_XP_CH  ADC_CHANNEL_3  // GPIO 4  (Trục X+, Phải) — giữ nguyên
 #define LDR_XM_CH  ADC_CHANNEL_4  // GPIO 5  (Trục X-, Trái) — giữ nguyên
 #define LDR_YP_CH  ADC_CHANNEL_6  // GPIO 7  (Trục Y+, Trên) ← ĐỔI
