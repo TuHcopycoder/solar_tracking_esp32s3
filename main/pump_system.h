626e8dac1aa3fa04dbac631dcddc569c3b72e687
TuHcopycoder <106220038@sv1.dut.udn.vn>
2026-05-09 16:17:39 +0700

    Update pump_system.h

--- main/pump_system.h
+++ main/pump_system.h
@@ -1,12 +1,19 @@
 #ifndef PUMP_SYSTEM_H
 #define PUMP_SYSTEM_H
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
 #include <stdbool.h>
 
-// Định nghĩa chân (Sửa lại số chân theo thực tế của bạn)
-#define PUMP_PIN 15 
+#define PUMP_PIN 18 
 
-void pump_init(); // Phải có dòng này
+// Khai báo các handle dùng chung
+extern QueueHandle_t pump_queue;
+extern SemaphoreHandle_t pump_mutex;
+
+void pump_init_rtos(void);
 void pump_control(bool state);
+void pump_task(void *pvParameters);
 
 #endif
