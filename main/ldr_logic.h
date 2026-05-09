e5746c90d2385293c9b8299965ded5fedaef0792
TuHcopycoder <106220038@sv1.dut.udn.vn>
2026-05-09 14:03:33 +0700

    Update ldr_logic.h

--- main/ldr_logic.h
+++ main/ldr_logic.h
@@ -4,10 +4,10 @@
 #include "esp_adc/adc_oneshot.h"
 
 
-#define LDR_XP_CH  ADC_CHANNEL_3  // GPIO 4  (Trục X+, Phải) — giữ nguyên
-#define LDR_XM_CH  ADC_CHANNEL_4  // GPIO 5  (Trục X-, Trái) — giữ nguyên
-#define LDR_YP_CH  ADC_CHANNEL_6  // GPIO 7  (Trục Y+, Trên) ← ĐỔI
-#define LDR_YM_CH  ADC_CHANNEL_7  // GPIO 8  (Trục Y-, Dưới) ← ĐỔI
+#define LDR_XP_CH  ADC_CHANNEL_3  // GPIO 4  (Trục X+, Phải)
+#define LDR_XM_CH  ADC_CHANNEL_4  // GPIO 5  (Trục X-, Trái) 
+#define LDR_YP_CH  ADC_CHANNEL_6  // GPIO 7  (Trục Y+, Trên) 
+#define LDR_YM_CH  ADC_CHANNEL_7  // GPIO 8  (Trục Y-, Dưới)
 
 void ldr_task(void *pvParameters); 
 
